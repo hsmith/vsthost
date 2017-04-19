888edfe2e5bc0e912f34d0aae6e14633deeb3059
jperek <jakub.perek@ii.uj.edu.pl>
2017-04-19 22:05:54 +0200

    wpf graphical interface implemented

--- vsthost_tester_csharp_wpf/VSTHostWindow.xaml.cs
+++ vsthost_tester_csharp_wpf/VSTHostWindow.xaml.cs
@@ -26,62 +26,270 @@ namespace vsthost_tester_csharp_wpf
     {
         private class HostObserverImpl : HostObserverProxyWrapper
         {
+            private VSTHostWindow parent;
+
+            public HostObserverImpl(VSTHostWindow p)
+            {
+                parent = p;
+            }
             public override void OnPluginAdded(UInt32 idx)
             {
-
+                parent.OnPluginAdded(idx);
             }
 
             public override void OnPluginDeleted(UInt32 idx)
             {
-
+                parent.OnPluginDeleted(idx);
             }
 
             public override void OnListLoaded()
             {
-
+                parent.OnListLoaded();
             }
 
             public override void OnMovedUp(UInt32 idx)
             {
-
+                parent.OnMovedUp(idx);
             }
 
             public override void OnMovedDown(UInt32 idx)
             {
-
+                parent.OnMovedDown(idx);
             }
 
             public override void OnEditorShown(UInt32 idx)
             {
-
+                parent.OnEditorShown(idx);
             }
 
             public override void OnEditorHidden(UInt32 idx)
             {
-
+                parent.OnEditorHidden(idx);
             }
 
             public override void OnPresetSet(UInt32 plugin_idx, UInt32 preset_idx)
             {
-
+                parent.OnPresetSet(plugin_idx, preset_idx);
             }
 
             public override void OnBypassSet(UInt32 idx, bool bypass)
             {
-
+                parent.OnBypassSet(idx, bypass);
             }
         }
+        private Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+        private HostObserverImpl hoi;
         private HostControllerProxy hcp;
+        private List<VSTPluginWindow> pluginWindows;
+
         public VSTHostWindow(HostControllerProxy hcp_)
         {
             InitializeComponent();
+            hoi = new HostObserverImpl(this);
             hcp = hcp_;
+            hcp.RegisterObserver(hoi);
+            
+            ofd.Filter = "VST Plugins (*.dll, *.vst3)|*.dll;*.vst3|VST2 Plugins (*.dll)|*.dll|VST3 Plugins (*.vst3)|*.vst3";
+            //ofd.InitialDirectory = "asdf";
+
+            Initialize();
+            SetButtons();
+        }
+
+        ~VSTHostWindow()
+        {
+            hcp.UnregisterObserver(hoi);
+        }
+
+        private int Selected()
+        {
+            return listPlugins.SelectedIndex;
+        }
+
+        private void Select(uint i)
+        {
+            listPlugins.SelectedIndex = Convert.ToInt32(i);
+            listPlugins.Focus();
+        }
+
+        private void Initialize()
+        {
+            pluginWindows = new List<VSTPluginWindow>();
+            listPlugins.Items.Clear();
+            for (uint i = 0; i < hcp.GetPluginCount(); ++i)
+            {
+                pluginWindows.Add(new VSTPluginWindow(hcp, i));
+                listPlugins.Items.Add(hcp.GetPluginName(i));
+            }
+            if (hcp.GetPluginCount() > 0)
+            {
+                Select(0);
+                SetButtons();
+            }
+        }
+
+        private void SetButtons()
+        {
+            var count = hcp.GetPluginCount();
+            var selected = Selected();
+
+            buttonUp.IsEnabled = count >= 2 && selected > 0;
+            buttonDown.IsEnabled = count >= 2 && selected >= 0 && selected < count - 1;
+            buttonDelete.IsEnabled = count > 0;
+
+            if (count > 0 && selected >= 0 && hcp.HasEditor(Convert.ToUInt32(selected)))
+            {
+                buttonShow.IsEnabled = true;
+                if (pluginWindows[selected].IsVisible == true)
+                    buttonShow.Content = "Hide";
+                else
+                    buttonShow.Content = "Show";
+            }
+            else
+            {
+                buttonShow.IsEnabled = false;
+                buttonShow.Content = "Show";
+            }
         }
 
         private void buttonShow_Click(object sender, RoutedEventArgs e)
         {
-            VSTPluginWindow pw = new VSTPluginWindow();
-            pw.Show();
+            var selected = Selected();
+            if (pluginWindows[selected].IsVisible == true)
+                pluginWindows[selected].HideEditor();
+            else
+                pluginWindows[selected].ShowEditor();
+        }
+
+        private void OnPluginAdded(UInt32 idx)
+        {
+            pluginWindows.Add(new VSTPluginWindow(hcp, idx));
+            listPlugins.Items.Add(hcp.GetPluginName(idx));
+            Select(idx);
+            SetButtons();
+        }
+
+        private void OnPluginDeleted(UInt32 idx)
+        {
+            var i = Convert.ToInt32(idx);
+            if (pluginWindows[i].IsVisible == true)
+                pluginWindows[i].HideEditor();
+            pluginWindows.RemoveAt(i);
+            listPlugins.Items.RemoveAt(i);
+
+            var count = hcp.GetPluginCount();
+            if (count > 0)
+            {
+                if (idx == count)
+                    Select(idx - 1);
+                else
+                    Select(idx);
+            }
+            else
+                listPlugins.SelectedIndex = -1;
+            SetButtons();
+        }
+
+        private void OnListLoaded()
+        {
+            Initialize();
+        }
+
+        private void OnMovedUp(UInt32 idx)
+        {
+            var i = Convert.ToInt32(idx);
+            pluginWindows[i].MovedUp();
+            pluginWindows[i - 1].MovedDown();
+
+            var tmp = pluginWindows[i];
+            pluginWindows[i] = pluginWindows[i - 1];
+            pluginWindows[i - 1] = tmp;
+
+            var tmp1 = listPlugins.Items[i];
+            listPlugins.Items[i] = listPlugins.Items[i - 1];
+            listPlugins.Items[i - 1] = tmp1;
+
+            Select(idx - 1);
+            SetButtons();
+        }
+
+        private void OnMovedDown(UInt32 idx)
+        {
+            var i = Convert.ToInt32(idx);
+            pluginWindows[i].MovedDown();
+            pluginWindows[i + 1].MovedUp();
+
+            var tmp = pluginWindows[i];
+            pluginWindows[i] = pluginWindows[i + 1];
+            pluginWindows[i + 1] = tmp;
+
+            var tmp1 = listPlugins.Items[i];
+            listPlugins.Items[i] = listPlugins.Items[i + 1];
+            listPlugins.Items[i + 1] = tmp1;
+
+            Select(idx + 1);
+            SetButtons();
+        }
+
+        private void OnEditorShown(UInt32 idx)
+        {
+            if (idx == Selected())
+                buttonShow.Content = "Hide";
+        }
+
+        private void OnEditorHidden(UInt32 idx)
+        {
+            if (idx == Selected())
+                buttonShow.Content = "Show";
+        }
+
+        private void OnPresetSet(UInt32 plugin_idx, UInt32 preset_idx)
+        {
+            pluginWindows[Convert.ToInt32(plugin_idx)].PresetSet(preset_idx);
+        }
+
+        private void OnBypassSet(UInt32 idx, bool bypass)
+        {
+            pluginWindows[Convert.ToInt32(idx)].BypassSet(bypass);
+        }
+
+        private void listPlugins_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SetButtons();
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var result = ofd.ShowDialog();
+            if (result == true)
+            {
+                hcp.AddPlugin(ofd.FileName);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            hcp.DeletePlugin(Convert.ToUInt32(Selected()));
+        }
+
+        private void buttonUp_Click(object sender, RoutedEventArgs e)
+        {
+            hcp.MoveUp(Convert.ToUInt32(Selected()));
+        }
+
+        private void buttonDown_Click(object sender, RoutedEventArgs e)
+        {
+            hcp.MoveDown(Convert.ToUInt32(Selected()));
+        }
+
+        private void buttonLoad_Click(object sender, RoutedEventArgs e)
+        {
+            hcp.LoadPluginList();
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            hcp.SavePluginList();
         }
     }
 }
--- vsthost_tester_csharp_wpf/VSTPluginWindow.xaml.cs
+++ vsthost_tester_csharp_wpf/VSTPluginWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using VSTHost;
+
 namespace vsthost_tester_csharp_wpf
 {
     /// <summary>
@@ -19,9 +21,157 @@ namespace vsthost_tester_csharp_wpf
     /// </summary>
     public partial class VSTPluginWindow : Window
     {
-        public VSTPluginWindow()
+        private HostControllerProxy hcp;
+        private uint index;
+
+        private System.Windows.Forms.Integration.WindowsFormsHost host = new System.Windows.Forms.Integration.WindowsFormsHost();
+        private System.Windows.Forms.UserControl uc = new System.Windows.Forms.UserControl();
+
+        private bool size_fixed = false;
+
+        public VSTPluginWindow(HostControllerProxy hcp_, uint i)
         {
             InitializeComponent();
+            hcp = hcp_;
+            index = i;
+
+            SizeToContent = SizeToContent.WidthAndHeight;
+            Title = hcp.GetPluginName(index);
+
+            hcp.CreateEditor(index, uc.Handle);
+            FixSize();
+            GeneratePresetMenu();
+        }
+
+        public void ShowEditor()
+        {
+            hcp.ShowEditor(index);
+            if (size_fixed == false)
+            {
+                FixSize();
+                size_fixed = true;
+            }
+            Show();
+        }
+
+        public void HideEditor()
+        {
+            hcp.HideEditor(index);
+            Hide();
+        }
+
+        public void MovedUp()
+        {
+            index--;
+        }
+
+        public void MovedDown()
+        {
+            index++;
+        }
+
+        public void BypassSet(bool bypass)
+        {
+            menuItemBypass.IsChecked = bypass;
+        }
+
+        public void ActiveSet(bool active)
+        {
+            menuItemActive.IsChecked = active;
+        }
+
+        public void PresetSet(uint idx)
+        {
+            var idx_ = Convert.ToInt32(idx);
+            for (int i = 0; i < menuItemPresets.Items.Count; ++i)
+                (menuItemPresets.Items[i] as MenuItem).IsChecked = i == idx_;
+        }
+
+        private void GeneratePresetMenu()
+        {
+            if (hcp.GetPluginPresetCount(index) > 0)
+            {
+                for (uint i = 0; i < hcp.GetPluginPresetCount(index); ++i)
+                {
+                    MenuItem item = new MenuItem();
+                    item.Header = hcp.GetPluginPresetName(index, i);
+                    item.Click += new RoutedEventHandler(menuItemPreset_Click);
+                    menuItemPresets.Items.Add(item);
+                }
+            }
+            else
+                menuItemPresets.IsEnabled = false;
+        }
+
+        private void FixSize()
+        {
+            var w = Convert.ToInt32(hcp.GetPluginEditorWidth(index));
+            var h = Convert.ToInt32(hcp.GetPluginEditorHeight(index));
+            if (w != uc.Width)
+            {
+                uc.Width = w;
+            }
+            if (w != grid.Width)
+            {
+                grid.Width = w;
+            }
+            if (h != uc.Height)
+            {
+                uc.Height = h;
+            }
+            if (h != grid.Height)
+            {
+                grid.Height = h;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            HideEditor();
+        }
+
+        private void menuItemBypass_Click(object sender, RoutedEventArgs e)
+        {
+            hcp.SetBypass(index, !menuItemBypass.IsChecked);
+        }
+
+        private void menuItemActive_Click(object sender, RoutedEventArgs e)
+        {
+            hcp.SetActive(index, !menuItemActive.IsChecked);
+        }
+
+        private void menuItemClose_Click(object sender, RoutedEventArgs e)
+        {
+            HideEditor();
+        }
+
+        private void menuItemLoad_Click(object sender, RoutedEventArgs e)
+        {
+            hcp.LoadPreset(index);
+        }
+
+        private void menuItemSave_Click(object sender, RoutedEventArgs e)
+        {
+            hcp.SavePreset(index);
+        }
+
+        private void menuItemPreset_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < menuItemPresets.Items.Count; ++i)
+            {
+                if (sender == menuItemPresets.Items[i])
+                {
+                    hcp.SetPluginPreset(index, Convert.ToUInt32(i));
+                    break;
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            host.Child = uc;
+            grid.Children.Add(host);
         }
     }
 }
