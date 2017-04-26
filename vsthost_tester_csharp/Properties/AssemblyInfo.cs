a727eb3778dbafd928a69281544c38cc02a3b882
jperek <jakub.perek@ii.uj.edu.pl>
2017-04-26 18:42:12 +0200

    fixed compilation warnings and csharp tester now closes properly

--- vsthost_tester_csharp/Properties/AssemblyInfo.cs
+++ vsthost_tester_csharp/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("vsthost_tester_csharp_wpf")]
+[assembly: AssemblyTitle("vsthost_tester_csharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("vsthost_tester_csharp_wpf")]
+[assembly: AssemblyProduct("vsthost_tester_csharp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
